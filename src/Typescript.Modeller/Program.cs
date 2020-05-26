cfbdb00845c0332ba6ef727d64b880ab27817e12
Gabriel Palma <gubpalma@hotmail.com>
2020-05-26 15:09:18 +1000

    Added program link

--- src/Typescript.Modeller/Program.cs
+++ src/Typescript.Modeller/Program.cs
@@ -6,9 +6,25 @@ namespace Typescript.Modeller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Initialising TypeScript converter");
 
+                if (args.Length != 2)
+                    throw new Exception("Expected two arguments <DLL File Path>, <Output Folder>");
 
+                new TypeScriptBuilder()
+                    .ConvertAsync(args[0], args[1])
+                    .Wait();
+
+                Console.WriteLine("Completed TypeScript convert");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                Environment.Exit(1);
+            }
         }
     }
 }
