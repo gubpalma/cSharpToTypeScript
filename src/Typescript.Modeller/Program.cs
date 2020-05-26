416d2d90a42380eb1ee573845b3f46957f50e528
Gabriel Palma <gubpalma@hotmail.com>
2020-05-26 15:52:57 +1000

    Added diagnostics

--- src/Typescript.Modeller/Program.cs
+++ src/Typescript.Modeller/Program.cs
@@ -8,16 +8,19 @@ namespace Typescript.Modeller
         {
             try
             {
-                Console.WriteLine("Initialising TypeScript converter");
+                Console.WriteLine("--------- Initialising TypeScript converter ---------");
 
                 if (args.Length != 2)
                     throw new Exception("Expected two arguments <DLL File Path>, <Output Folder>");
+
+                Console.WriteLine($"Assembly Path: {args[0]}");
+                Console.WriteLine($"Output Path: {args[1]}");
 
                 new TypeScriptBuilder()
                     .ConvertAsync(args[0], args[1])
                     .Wait();
 
-                Console.WriteLine("Completed TypeScript convert");
+                Console.WriteLine("--------- Completed TypeScript conversion ---------");
             }
             catch (Exception e)
             {
